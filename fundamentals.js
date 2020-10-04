1f4fd58abeaed8898ec9c603990e353b85f33c72
Yuxuan Wang <2879296442@qq.com>
2020-10-04 17:27:05 +0100

    Arrays

--- fundamentals.js
+++ fundamentals.js
@@ -1,5 +1,9 @@
 const aCar = {
     owner: "Joe Bloggs",
+    previous_owners: [
+        { name: "Pat Smith", address: "1 Main Street" },
+        { name: "Sheila Dwyer", address: "2 High Street" }
+      ],
     type: {
       make: "Toyota",
       model: "Corolla",
@@ -32,3 +36,9 @@ console.log(
     aCar.color.interior.texture +
     " interior."
 );
+console.log(
+    "First owner: " +
+      aCar.previous_owners[0].name +
+      ' - ' +
+      aCar.previous_owners[0].address
+  )
